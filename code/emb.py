ee8c2179b8487526a4e70f84bba83a21e080f080
Sachchithananthan Gowreeshan <gowreeshan1@gmail.com>
2024-02-29 18:12:48 +0530

    Add files via upload

---
+++ code/emb.py
@@ -0,0 +1,187 @@
+from __future__ import print_function
+
+import argparse
+import collections
+import datetime
+import os
+import pprint
+import random
+import sys
+
+import dateutil.tz
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+
+import CoresetGreedy
+from datasets import TextDataset
+from miscc.config import cfg, cfg_from_file
+from model import RNN_ENCODER, CNN_ENCODER
+
+dir_path = (os.path.abspath(os.path.join(os.path.realpath(__file__), './.')))
+sys.path.append(dir_path)
+
+
+def prepare_data(data):
+    imgs, captions, captions_lens, class_ids, keys = data
+
+    # sort data by the length in a decreasing order
+    sorted_cap_lens, sorted_cap_indices = \
+        torch.sort(captions_lens, 0, True)
+
+    real_imgs = []
+    for i in range(len(imgs)):
+        imgs[i] = imgs[i][sorted_cap_indices]
+        if cfg.CUDA:
+            real_imgs.append(Variable(imgs[i]).cuda())
+        else:
+            real_imgs.append(Variable(imgs[i]))
+
+    captions = captions[sorted_cap_indices].squeeze()
+    class_ids = class_ids[sorted_cap_indices].numpy()
+    # sent_indices = sent_indices[sorted_cap_indices]
+    keys = [keys[i] for i in sorted_cap_indices.numpy()]
+    # print('keys', type(keys), keys[-1])  # list
+    if cfg.CUDA:
+        captions = Variable(captions).cuda()
+        sorted_cap_lens = Variable(sorted_cap_lens).cuda()
+    else:
+        captions = Variable(captions)
+        sorted_cap_lens = Variable(sorted_cap_lens)
+
+    return [real_imgs, captions, sorted_cap_lens,
+            class_ids, keys]
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a AttnGAN network')
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file',
+                        default='cfg/bird_attn2.yml', type=str)
+    parser.add_argument('--gpu', dest='gpu_id', type=int, default=-1)
+    parser.add_argument('--data_dir', dest='data_dir', type=str, default='')
+    parser.add_argument('--manualSeed', type=int, help='manual seed')
+    args = parser.parse_args()
+    return args
+
+
+batch_size = 20
+
+if __name__ == "__main__":
+    args = parse_args()
+    if args.cfg_file is not None:
+        cfg_from_file(args.cfg_file)
+
+    if args.gpu_id != -1:
+        cfg.GPU_ID = args.gpu_id
+    else:
+        cfg.CUDA = False
+
+    if args.data_dir != '':
+        cfg.DATA_DIR = args.data_dir
+    print('Using config:')
+    pprint.pprint(cfg)
+
+    if not cfg.TRAIN.FLAG:
+        args.manualSeed = 100
+    elif args.manualSeed is None:
+        args.manualSeed = random.randint(1, 10000)
+    random.seed(args.manualSeed)
+    np.random.seed(args.manualSeed)
+    torch.manual_seed(args.manualSeed)
+    if cfg.CUDA:
+        torch.cuda.manual_seed_all(args.manualSeed)
+
+    now = datetime.datetime.now(dateutil.tz.tzlocal())
+    timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+    output_dir = '../output/%s_%s_%s' % \
+                 (cfg.DATASET_NAME, cfg.CONFIG_NAME, timestamp)
+
+    split_dir, bshuffle = 'train', True
+    if not cfg.TRAIN.FLAG:
+        # bshuffle = False
+        split_dir = 'test'
+
+    # Get data loader
+    imsize = cfg.TREE.BASE_SIZE * (2 ** (cfg.TREE.BRANCH_NUM - 1))
+    image_transform = transforms.Compose([
+        transforms.Resize(int(imsize * 76 / 64)),
+        transforms.RandomCrop(imsize),
+        transforms.RandomHorizontalFlip()])
+    dataset = TextDataset(cfg.DATA_DIR, split_dir,
+                          base_size=cfg.TREE.BASE_SIZE,
+                          transform=image_transform)
+    assert dataset
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=cfg.TRAIN.BATCH_SIZE,
+        drop_last=True, shuffle=bshuffle, num_workers=int(cfg.WORKERS))
+
+    if cfg.TRAIN.NET_E == '':
+        print('Error: no pretrained text-image encoders')
+        exit(0)
+
+    image_encoder = CNN_ENCODER(cfg.TEXT.EMBEDDING_DIM)
+    img_encoder_path = cfg.TRAIN.NET_E.replace('text_encoder', 'image_encoder')
+    state_dict = \
+        torch.load(img_encoder_path, map_location=lambda storage, loc: storage)
+    image_encoder.load_state_dict(state_dict)
+    for p in image_encoder.parameters():
+        p.requires_grad = False
+    print('Load image encoder from:', img_encoder_path)
+    image_encoder.eval()
+
+    text_encoder = \
+        RNN_ENCODER(dataset.n_words, nhidden=cfg.TEXT.EMBEDDING_DIM)
+    state_dict = \
+        torch.load(cfg.TRAIN.NET_E,
+                   map_location=lambda storage, loc: storage)
+    text_encoder.load_state_dict(state_dict)
+    for p in text_encoder.parameters():
+        p.requires_grad = False
+    print('Load text encoder from:', cfg.TRAIN.NET_E)
+    text_encoder.eval()
+
+    ##original##############
+    
+    # data_iter = iter(self.data_loader)
+    # step = 0
+    #  while step < self.num_batches:
+    #   data = next(data_iter) #data = data_iter.next()
+    #   imgs, captions, cap_lens, class_ids, keys = prepare_data(data)
+    #   hidden = text_encoder.init_hidden(batch_size)
+    #   words_embs, sent_emb = text_encoder(captions, cap_lens, hidden)
+    #   words_embs, sent_emb = words_embs.detach(), sent_emb.detach()
+
+    ######################################
+
+    data_groups = collections.defaultdict(list)
+    for data in dataloader:
+        imgs, captions, cap_lens, class_ids, keys = prepare_data(data)
+        # print(type(imgs[0]))
+        for img, caption, cap_len, class_id, key in zip(imgs, captions, cap_lens, class_ids, keys):
+            data_groups[class_id].append([img, caption, cap_len, key])
+
+    coreset_list = []
+    for class_id, group_data in data_groups.items():
+        print(len(group_data))
+        modified_group_data = []  # Store modified data with class ID
+        imgslist, captionslist, cap_lenslist, class_idslist, keyslist = [],[],[],[],[]
+        for img, caption, cap_len, key in group_data:
+            print(type(caption))
+            imgslist.append(img)
+            captionslist.append(caption)
+            cap_lenslist.append(cap_len)
+            keyslist.append(key)
+
+        hidden = text_encoder.init_hidden(len(imgslist))
+        words_embs, sent_emb = text_encoder(captionslist, cap_lenslist, hidden)
+        words_embs, sent_emb = words_embs.detach(), sent_emb.detach()
+
+        indexes = CoresetGreedy.CoresetGreedy(words_embs).sample(0.1)
+        coreset_list.append(indexes)
+        print(indexes)
+
+    print("Coreset List: ")
+    print(coreset_list)
